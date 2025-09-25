e51c99d4abc4ba8add01e01f617b14a81fceb320
Fernando Augusto Costa Pereira <fernandoaugustocostapereira@gmial.com>
2025-09-25 15:00:20 -0400

    testado importação de certificado

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, abort
+from flask_cors import CORS
+import requests
+import uuid
+from validate_docbr import CPF, CNPJ
+import re
+from flask import Response
+import json
+import logging
 
 app = Flask(__name__)
 
@@ -10,5 +18,20 @@ def home():
 def health():
     return jsonify({"status": "ok"})
 
+# ===============================
+# 🔑 Certificado único (global)
+# ===============================
+CERT = (
+    "./certs/hom_cert_ciamara.crt",
+    "./certs/hom_key_ciamara.key"
+)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+ 
